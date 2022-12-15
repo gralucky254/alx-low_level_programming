255aa12745527f86fbc3a572e263e10b2e749408
root <hehomichael275@gmail.com>
2022-12-15 05:43:21 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @n: checks absolute value of an interger
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n * 1);
+	}
+	return (n);
+}
